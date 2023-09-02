96590852478347145cc3ab588b240a172254e17b
Ranjithdnv <rith8596@gmail.com>
2023-09-02 13:12:01 +0530

    socket

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Server } = require( "socket.io")
 
 const io = new Server({
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://soclienttest.onrender.com/",
   },
 });
 let onlineUsers = [];
