ad8114c0f22defab3323362dae16033bec8402ff
Ranjithdnv <rith8596@gmail.com>
2023-09-02 15:45:29 +0530

    socket

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
 const { Server } = require( "socket.io")
 const express = require('express')
 const app = express()
-const io = new Server({
-  cors: {
-    origin: "https://soclienttest.onrender.com:3000",
-  },
-});
-let onlineUsers = [];
+ //const io = new Server({
+//   cors: {
+//     origin: "https://soclienttest.onrender.com:3000",
+//   },
+// });
+// let onlineUsers = [];
 
-const addNewUser = (username, socketId) => {
-  !onlineUsers.some((user) => user.username === username) &&
-    onlineUsers.push({ username, socketId });
-};
-const getUser = (username) => {
-  return onlineUsers.find((user) => user.username === username);
-};
-const removeUser = (socketId) => {
-  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
-};
+// const addNewUser = (username, socketId) => {
+//   !onlineUsers.some((user) => user.username === username) &&
+//     onlineUsers.push({ username, socketId });
+// };
+// const getUser = (username) => {
+//   return onlineUsers.find((user) => user.username === username);
+// };
+// const removeUser = (socketId) => {
+//   onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+// };
 
-io.on("connection", (socket) => {
-  socket.on("newUser", (username) => {
-    addNewUser(username, socket.id);
-
-
-
-    console.log(onlineUsers)
-  })
-  socket.on("sendText", ({ senderName, receiverName, text }) => {
-    const receiver = getUser(receiverName);
-    console.log(receiver)
-    console.log(senderName)
-    console.log(receiverName)
-    console.log(text)
+// io.on("connection", (socket) => {
+//   socket.on("newUser", (username) => {
+//     addNewUser(username, socket.id);
 
 
-    io.to(receiver?.socketId).emit("getText", {
-      senderName,
-      text,
-    });
-  });
+
+//     console.log(onlineUsers)
+//   })
+//   socket.on("sendText", ({ senderName, receiverName, text }) => {
+//     const receiver = getUser(receiverName);
+//     console.log(receiver)
+//     console.log(senderName)
+//     console.log(receiverName)
+//     console.log(text)
+
+
+//     io.to(receiver?.socketId).emit("getText", {
+//       senderName,
+//       text,
+//     });
+//   });
  
-  socket.on("disconnect", () => {
-    removeUser(socket.id);
-    console.log(onlineUsers)
+//   socket.on("disconnect", () => {
+//     removeUser(socket.id);
+//     console.log(onlineUsers)
    
-  });
+//   });
 
-});
+// });
 
 app.get('/',async (req, res) => {
   
