99725cbf69744f3fb0d8039f15677de74b71c503
Ranjithdnv <rith8596@gmail.com>
2023-09-02 12:52:05 +0530

    socket

---
+++ index.js
@@ -0,0 +1,52 @@
+const { Server } = require( "socket.io")
+
+const io = new Server({
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+let onlineUsers = [];
+
+const addNewUser = (username, socketId) => {
+  !onlineUsers.some((user) => user.username === username) &&
+    onlineUsers.push({ username, socketId });
+};
+const getUser = (username) => {
+  return onlineUsers.find((user) => user.username === username);
+};
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+io.on("connection", (socket) => {
+  socket.on("newUser", (username) => {
+    addNewUser(username, socket.id);
+
+
+
+    console.log(onlineUsers)
+  })
+  socket.on("sendText", ({ senderName, receiverName, text }) => {
+    const receiver = getUser(receiverName);
+    console.log(receiver)
+    console.log(senderName)
+    console.log(receiverName)
+    console.log(text)
+
+
+    io.to(receiver?.socketId).emit("getText", {
+      senderName,
+      text,
+    });
+  });
+ 
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+    console.log(onlineUsers)
+   
+  });
+
+});
+
+
+  io.listen(5000);
