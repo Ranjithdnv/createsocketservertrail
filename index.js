158d6f5bcb88c951db3945541c40783a65b6eb94
Ranjithdnv <rith8596@gmail.com>
2023-09-06 13:40:22 +0530

    socket

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-// const { Server } = require( "socket.io")
-// const express = require('express')
-// const app = express()
-//  const io = new Server({
+// const { Server } = require("socket.io");
+// const express = require("express");
+// const app = express();
+// const io = new Server({
 //   cors: {
-//     origin: "https://soclienttest.onrender.com:3002",
+//     origin: "http://localhost:3000",
 //   },
 // });
+//---
 const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://future-together.onrender.com", //https://future-together.onrender.com    //https://soclienttest.onrender.com
+    origin: "https://future-together.onrender.com ", //https://future-together.onrender.com    //https://soclienttest.onrender.com
     methods: ["GET", "POST"],
   },
 });
@@ -36,18 +37,17 @@ io.on("connection", (socket) => {
 
     console.log(onlineUsers);
   });
-  socket.on("sendText", ({ senderName, receiverName, text }) => {
-    const receiver = getUser(receiverName);
-    console.log(receiver);
-    console.log(senderName);
-    console.log(receiverName);
-    console.log(text);
+  socket.on("sendText", ({ message, mname, mid }) => {
+    const receiver = getUser(mname);
+    console.log(message);
+    console.log(mname);
+    console.log(mid);
+    // console.log(text);
 
     io.to(receiver?.socketId).emit("getText", {
-      senderName,
-      mid: "1",
-      mname: "ranjithkumar",
-      mname: text,
+      message: message,
+      mname: mname,
+      mid: mid,
     });
   });
 
@@ -61,6 +61,6 @@ app.get("/", async (req, res) => {
   res.send("success");
 });
 
-// io.listen(3002);
+// io.listen(5000);
 
 server.listen(5000, () => console.log("server is running on port 5000"));
